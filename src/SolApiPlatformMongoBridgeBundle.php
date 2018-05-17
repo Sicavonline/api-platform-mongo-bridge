7cc723cde00021751764dbaa79d1cb6c44dd7995
Alexandre CLOTAIL <a.clotail@sicavonline.fr>
2018-05-17 09:49:14 +0000

    clean code

--- src/SolApiPlatformMongoBridgeBundle.php
+++ src/SolApiPlatformMongoBridgeBundle.php
@@ -5,13 +5,12 @@ namespace Sol\ApiPlatform\MongoBridge;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+/**
+ * Class SolApiPlatformMongoBridgeBundle
+ * @package Sol\ApiPlatform\MongoBridge
+ */
 class SolApiPlatformMongoBridgeBundle extends Bundle
 {
-    public function __construct()
-    {
-        // die('ok');
-    }
-
     /**
      * {@inheritdoc}
      */
